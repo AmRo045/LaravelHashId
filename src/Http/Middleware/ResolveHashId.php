fbeeb12e6ec892da3f730239a5f806c3061559c8
AmRo <virtualemailbox045@gmail.com>
2023-02-18 11:51:52 +0330

    Add ability to ignore or decode specific parameters

--- src/Http/Middleware/ResolveHashId.php
+++ src/Http/Middleware/ResolveHashId.php
@@ -16,11 +16,33 @@ class ResolveHashId
         $this->hashids = $hashids;
     }
 
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, ?string $parameters = ''): Response
+    {
+        if (static::hasIgnoredParameters($parameters)) {
+            $this->decode($request, function ($parameterName) use ($parameters) {
+                $parametersToIgnore = static::getIgnoredParameters($parameters);
+                return in_array($parameterName, $parametersToIgnore);
+            });
+        } else if (static::hasOnlyParameters($parameters)) {
+            $this->decode($request, function ($parameterName) use ($parameters) {
+                $parametersToDecode = static::getOnlyParameters($parameters);
+                return !in_array($parameterName, $parametersToDecode);
+            });
+        } else {
+            $this->decode($request);
+        }
+
+        return $next($request);
+    }
+
+    private function decode(Request $request, ?Closure $skipCondition = null): void
     {
         $route = $request->route();
 
-        foreach($route->parameters() as $key => $value) {
+        foreach ($route->parameters() as $key => $value) {
+            if ($skipCondition && $skipCondition($key))
+                continue;
+
             $decodedValue = $this->hashids->decode($value);
 
             if (!$decodedValue)
@@ -30,7 +52,35 @@ class ResolveHashId
 
             $route->setParameter($key, $decodedValue);
         }
+    }
 
-        return $next($request);
+    private static function hasIgnoredParameters(string $parameters): bool
+    {
+        return str_starts_with($parameters, 'ignore');
+    }
+
+    private static function hasOnlyParameters(string $parameters): bool
+    {
+        return str_starts_with($parameters, 'only');
+    }
+
+    private static function getIgnoredParameters(string $parameters): array
+    {
+        return static::extractParameters($parameters);
+    }
+
+    private static function getOnlyParameters(string $parameters): array
+    {
+        return static::extractParameters($parameters);
+    }
+
+    private static function extractParameters(string $parameters): array
+    {
+        if (!$parameters)
+            return [];
+
+        $actualParameters = explode('=', $parameters)[1];
+
+        return explode('&', $actualParameters);
     }
 }
